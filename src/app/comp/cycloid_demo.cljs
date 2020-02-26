36e5448ec72858a8808d63ea320e8c89b787afbb
jiyinyiyong <jiyinyiyong@gmail.com>
2020-02-27 01:20:30 +0800

    fix math in drawing cycloid

--- src/app/comp/cycloid_demo.cljs
+++ src/app/comp/cycloid_demo.cljs
@@ -13,19 +13,19 @@
   {}
   (graphics
    {:position [400 400],
-    :ops (let [r1 (+ 20 (rand 400))
-               r2 (+ 20 (rand 400))
-               r3 (+ 20 (rand 400))
+    :ops (let [r1 (rand-int 400)
+               r2 (rand-int 400)
+               r3 (rand-int 400)
                tt1 (rand 6)
                tt2 (rand 6)
-               trail (->> (range 10000)
+               trail (->> (range 8000)
                           (map
                            (fn [idx]
-                             (let [t (* idx 0.05)
+                             (let [t (* idx 0.11)
                                    dr (- r1 r2)
                                    dr2 (- r2 r3)
                                    t2 (+ tt1 (unchecked-negate (/ (* t r1) r2)))
-                                   t3 (+ tt2 (unchecked-negate (/ (* t2 r3) r3)))]
+                                   t3 (+ tt2 (unchecked-negate (/ (* t2 r2) r3)))]
                                (add-path
                                 (add-path
                                  [(* dr (js/Math.cos t)) (* dr (js/Math.sin t))]
@@ -33,7 +33,7 @@
                                 [(* r3 (js/Math.cos t3)) (* r3 (js/Math.sin t3))])))))]
       (vec
        (concat
-        [(g :line-style {:color (rand-int (hslx 0 0 100)), :width 2, :alpha 0.8})
+        [(g :line-style {:color (rand-int (hslx 0 0 100)), :width 1, :alpha 0.8})
          (g :move-to (first trail))]
         (->> trail rest (mapcat (fn [p] [(g :line-to p)]))))))})
   (comp-reset [-40 40])))
