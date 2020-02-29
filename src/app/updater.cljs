f19a7abc356604bb0a58f2846c22f2000975b84a
jiyinyiyong <jiyinyiyong@gmail.com>
2020-02-29 21:18:59 +0800

    remove duplicated updater function

--- src/app/updater.cljs
+++ src/app/updater.cljs
@@ -2,4 +2,10 @@
 (ns app.updater )
 
 (defn updater [store op op-data op-id op-time]
-  (case op :content (assoc store :content op-data) :hydrate-storage op-data store))
+  (case op
+    :tab (assoc store :tab op-data)
+    :touch (assoc store :touch-key op-id)
+    :states
+      (let [[cursor new-state] op-data, path (conj cursor :data)]
+        (assoc-in store (concat [:states] path) new-state))
+    (do (println "unknown op" op op-data) store)))
